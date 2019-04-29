2df24cdd98c3e122ba3c8a447862ffa166247115
Jean Marcel Pripko <jeanpripko@hotmail.com>
2019-04-29 19:26:03 -0300

    Correção apps

--- AC_DJANGO/AC_DJANGO/settings.py
+++ AC_DJANGO/AC_DJANGO/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'alunos',
-    'disciplinas',
-    'professores',
+    'CORE',
 ]
 
 MIDDLEWARE = [
