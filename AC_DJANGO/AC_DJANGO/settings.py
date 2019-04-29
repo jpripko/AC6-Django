17e223bf0ff887592ae0f261ce8e46337e6fb9e7
Jean Marcel Pripko <jeanpripko@hotmail.com>
2019-04-29 10:58:15 -0300

    add apps no settings

--- AC_DJANGO/AC_DJANGO/settings.py
+++ AC_DJANGO/AC_DJANGO/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'alunos',
+    'disciplinas',
+    'professores',
 ]
 
 MIDDLEWARE = [
