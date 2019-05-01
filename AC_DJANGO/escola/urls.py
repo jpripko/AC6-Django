d68f75b679f15c9ce51423802740874a45555e01
Caio Oliviera <caio.ribeiro@aluno.faculdadeimpacta.com.br>
2019-05-01 12:11:53 -0300

    Alterações gerais

--- AC_DJANGO/escola/urls.py
+++ AC_DJANGO/escola/urls.py
@@ -1,4 +1,4 @@
-"""AC_DJANGO URL Configuration
+"""escola URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('',index),
 ]
--- AC_DJANGO/manage.py
+++ AC_DJANGO/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AC_DJANGO.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escola.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
